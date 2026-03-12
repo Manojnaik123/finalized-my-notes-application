2312b7ae75bf3957537628c246696c0f3a41ae65
Manojnaik123 <manojnaikbse@gmail.com>
2026-03-12 11:23:35 +0530

    mobile bread crumb - added libraryid in the params

--- components/app/mobile/breadcrumb.tsx
+++ components/app/mobile/breadcrumb.tsx
@@ -7,14 +7,28 @@ import {
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
 import { folders } from "@/delete-later/data";
+import { FOLDERS_KEY, NOTES_KEY } from "@/lib/query-keys/query-keyx";
+import { Note } from "@/types/main-types/note";
+import { Folder } from "@/types/props-types/folder.";
+import { useQueryClient } from "@tanstack/react-query";
 import { useParams } from "next/navigation"
 
 export function BreadcrumbMobile() {
-  const { folderId, libraryId } = useParams()
+  const { folderId, libraryId, noteId } = useParams()
 
   const numericFolderId = Number(folderId)
+  const numericNoteId = Number(noteId)
 
-  const curFolder = folders.find((folder) => folder.id === numericFolderId);
+  const queryClient = useQueryClient()
+
+  const curFolder = queryClient.getQueryData<Folder[]>([FOLDERS_KEY])?.find((folder) => folder.id === numericFolderId)
+  const curNote = queryClient.getQueryData<Note[]>([NOTES_KEY, numericFolderId])?.find((note) => note.id === numericNoteId)
+
+
+  console.log('from breadcrum')
+  console.log(curNote);
+  
+  
 
   return (
     <Breadcrumb className="">
@@ -24,11 +38,11 @@ export function BreadcrumbMobile() {
         </BreadcrumbItem>
         <BreadcrumbSeparator /> */}
         <BreadcrumbItem>
-          <BreadcrumbLink href={`/mynotes/dashboard/${libraryId}/${folderId}`}>{curFolder?.title}</BreadcrumbLink>
+          <BreadcrumbLink href={`/mynotes/dashboard/${libraryId}/${folderId}`}>{curFolder?.folder_name}</BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
         <BreadcrumbItem>
-          <BreadcrumbPage>Page</BreadcrumbPage>
+          <BreadcrumbPage>{curNote?.title}</BreadcrumbPage>
         </BreadcrumbItem>
       </BreadcrumbList>
     </Breadcrumb>
