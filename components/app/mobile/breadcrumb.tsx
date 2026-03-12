dc53b22a8bf740248bfb97a148aeb29274ab68c4
Manojnaik123 <manojnaikbse@gmail.com>
2026-03-12 11:12:09 +0530

    mobile bread crumb - added libraryid in the params

--- components/app/mobile/breadcrumb.tsx
+++ components/app/mobile/breadcrumb.tsx
@@ -24,7 +24,7 @@ export function BreadcrumbMobile() {
         </BreadcrumbItem>
         <BreadcrumbSeparator /> */}
         <BreadcrumbItem>
-          <BreadcrumbLink href={`/mynotes/dashboard/${libraryId}/${curFolder?.id}`}>{curFolder?.title}</BreadcrumbLink>
+          <BreadcrumbLink href={`/mynotes/dashboard/${libraryId}/${folderId}`}>{curFolder?.title}</BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator />
         <BreadcrumbItem>
